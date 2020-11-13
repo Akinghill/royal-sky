765af1abcffb416ca435c17d6482083d91913378
akinghill <austinkinghill@gmail.com>
2020-11-13 12:55:46 -0700

    Updates

---
+++ precache-manifest.06203cf69e351ed6347dd84633b28b41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d56349fe76c77702b6936f611cae8fe",
+    "url": "/royal-sky/index.html"
+  },
+  {
+    "revision": "297922ef68767386a4dc",
+    "url": "/royal-sky/static/css/main.07914211.chunk.css"
+  },
+  {
+    "revision": "87a5c6564f8aadc230c7",
+    "url": "/royal-sky/static/js/2.e35d2604.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/royal-sky/static/js/2.e35d2604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297922ef68767386a4dc",
+    "url": "/royal-sky/static/js/main.d9a766d8.chunk.js"
+  },
+  {
+    "revision": "50eeff2413b19ea5c17e",
+    "url": "/royal-sky/static/js/runtime-main.0d7e7763.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/royal-sky/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/royal-sky/static/media/shopping-bag.30f07071.svg"
+  }
+]);
