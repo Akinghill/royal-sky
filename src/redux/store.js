100b16adf23486f5b7054ee87fdf0af90d146756
akinghill <austinkinghill@gmail.com>
2020-11-30 19:12:52 -0700

    production

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,8 +10,9 @@ if(process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
 }
 
-
-
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
-
 export const persistor = persistStore(store)
+// const store = createStore(
+//   reducer, /* preloadedState, */
+// +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
