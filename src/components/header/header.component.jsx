3323cd74b79d4a06cb357d250027797bf1c7816d
akinghill <austinkinghill@gmail.com>
2020-12-27 17:05:25 -0700

    fix nav pathnames

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,17 +12,17 @@ import './header.styles.scss'
 
 const Header = ({ currentUser, hidden }) => (
   <div className="header">
-    <Link to="/royal-sky">
+    <Link to="/">
       <Logo className="logo" />
     </Link>
     <div className="options">
-      <Link className="option" to="/royal-sky/shop">SHOP</Link>
-      <Link className="option" to="/royal-sky/shop">CONTACT</Link>
+      <Link className="option" to="/shop">SHOP</Link>
+      <Link className="option" to="/shop">CONTACT</Link>
       {
         currentUser ?
           <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
           :
-          <Link className="option" to="/royal-sky/signin">SIGN IN</Link>
+          <Link className="option" to="/signin">SIGN IN</Link>
       }
       <CartIcon />
     </div>
