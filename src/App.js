1e30383c9f9b92f17ca2d8e8bf835087073cf035
akinghill <austinkinghill@gmail.com>
2021-01-09 19:47:11 -0700

    upload data to firestore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -9,14 +10,17 @@ import ShopPage from './pages/shop/shop.component';
 import CheckoutPage from './pages/checkout/checkout.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.util'
-import { setCurrentUser } from './redux/user/user.actions'
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.util'
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -31,6 +35,10 @@ class App extends React.Component {
       } else {
         setCurrentUser(userAuth)
       }
+      addCollectionAndDocuments(
+        'collections',
+        collectionsArray.map(({ title, items }) => ({ title, items }))
+      )
     })
   }
 
@@ -61,10 +69,10 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = (user) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview,
 })
-
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -14,13 +14,14 @@ const config = {
 }
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth) return;
+  if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`)
 
   const snapShot = await userRef.get()
 
-  if(!snapShot.exists) {
+
+  if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date()
 
@@ -35,10 +36,23 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
       console.log('error ceating user', error.message);
     }
   }
-
   return userRef
-
 }
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
 
 firebase.initializeApp(config)
 
