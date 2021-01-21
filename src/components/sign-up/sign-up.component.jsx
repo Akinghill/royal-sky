6aea1c229084681bb5b193effe445ab1935a1481
akinghill <austinkinghill@gmail.com>
2021-01-20 17:32:09 -0700

    sign up sagas

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,97 +1,96 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
-import FormInput from '../form-input/form-input.component'
-import CustomButton from '../custom-button/custom-button.component'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
+import { signUpStart } from '../../redux/user/user.actions';
 
-import './sign-up.styles.scss'
+import './sign-up.styles.scss';
 
-const SignUp = () => {
+const SignUp = (props) => {
   const [userCredentials, setUserCredentials] = useState({
     displayName: '',
     email: '',
     password: '',
-    confirmPassword: ''
-  })
+    confirmPassword: '',
+  });
 
-  const { displayName, email, password, confirmPassword } = userCredentials
+  const { displayName, email, password, confirmPassword } = userCredentials;
 
-  const handleSubmit = async event => {
-    event.preventDefault()
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { signUpStart } = props;
 
     if (password !== confirmPassword) {
-      alert('Passwords do not match')
-      return
+      alert('Passwords do not match');
+      return;
     }
 
-    try {
-      const { user } = await auth.createUserWithEmailAndPassword(email, password)
+    signUpStart({ displayName, email, password });
 
-      await createUserProfileDocument(user, { displayName })
-
-      setUserCredentials({
-        displayName: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-      })
-    } catch (error) {
-      console.error(error)
-    }
-
-  }
+    setUserCredentials({
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+  };
 
   const handleChange = (event) => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
     setUserCredentials({
       ...userCredentials,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   return (
-    <div className="sign-up">
-      <h2 className="title">I do not have an account</h2>
+    <div className='sign-up'>
+      <h2 className='title'>I do not have an account</h2>
       <span>Sign up with your email and password</span>
-      <form onSubmit={handleSubmit} className="sign-up-form">
+      <form onSubmit={handleSubmit} className='sign-up-form'>
         <FormInput
-          type="text"
-          name="displayName"
+          type='text'
+          name='displayName'
           value={displayName}
           onChange={handleChange}
           label='Display Name'
           required
         />
         <FormInput
-          type="email"
-          name="email"
+          type='email'
+          name='email'
           value={email}
           onChange={handleChange}
           label='Email'
           required
         />
         <FormInput
-          type="password"
-          name="password"
+          type='password'
+          name='password'
           value={password}
           onChange={handleChange}
           label='Password'
           required
         />
         <FormInput
-          type="password"
-          name="confirmPassword"
+          type='password'
+          name='confirmPassword'
           value={confirmPassword}
           onChange={handleChange}
           label='Confirm Password'
           required
         />
-        <CustomButton type="submit">SIGN UP</CustomButton>
+        <CustomButton type='submit'>SIGN UP</CustomButton>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+const mapDispatchToProps = (dispatch) => ({
+  signUpStart: (userCredentials) => dispatch(signUpStart(userCredentials)),
+});
+
+export default connect(null, mapDispatchToProps)(SignUp);
