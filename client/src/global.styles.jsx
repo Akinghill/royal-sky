7704d2680627944ac818e229885993a0b10df8f9
akinghill <austinkinghill@gmail.com>
2021-01-27 12:07:23 -0700

    update gitignore and convert styled components

--- client/src/global.styles.jsx
+++ client/src/global.styles.jsx
@@ -1,21 +1,18 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: 'Open Sans Condensed';
-    padding: 20px 40px;
-
-    @media screen and (max-width 800px) {
-      paadding: 10px
-    }
-  }
-
-  a {
-    text-decoration: none;
-    color: black;
-  }
-`
+	body {
+		font-family: 'Open Sans Condensed';
+		padding: 20px 40px;
+		@media screen and (max-width: 800px) {
+			padding: 10px;
+		}
+	}
+	a {
+		text-decoration: none;
+		color: black;
+	}
+	* {
+		box-sizing: border-box;
+	}
+`;
